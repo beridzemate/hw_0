94a2db5c89bdeb706e7a6596badc67648ab31164
beridzemate <mateberidze464@gmail.com>
2024-09-28 16:47:55 +0400

    uptade commit

---
+++ day138/classwork/ErrorBoundary.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ErrorBoundary from './ErrorBoundary';
+import MyComponent from './MyComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <ErrorBoundary>
+        <MyComponent />
+      </ErrorBoundary>
+    </div>
+  );
+}
+
+export default App;
+
